d88aa396c3fb5bbd3a0f1755eac64db19e89a816
psep <psep@debian.cl>
2014-07-05 23:20:08 -0400

    initial commit

---
+++ images/mini/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright (C) 2014 Pablo Sepúlveda P. <pablo.sepulveda@ti-nova.cl>
+ *
+ * This file is part of tinova-web.
+ * tinova-web is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * tinova-web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the␊
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tinova-web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+header("Location: ../");
+?>
